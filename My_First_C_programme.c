dac83c7cdef4dae5da0b387ec3c90d7546744ff7
Koina Rautkar <koinarautkar12@gmail.com>
2026-08-10 21:02:02 +0530

    first commit - hello world and addition program

---
+++ My_First_C_programme.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the value of a: ");
+    scanf("%d",&a);
+
+    printf("enter the value of b: ");
+    scanf("%d",&b);
+
+    printf("a= %d \n",a);
+    printf("b= %d \n",b);
+    printf("a+b= %d",a+b);
+    return 0;
+}
