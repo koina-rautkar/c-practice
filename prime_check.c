62f3658e09e219c2fd004a4b25c716726c014bb7
Koina Rautkar <koinarautkar12@gmail.com>
2026-09-11 22:38:35 +0530

    isPrime function

---
+++ prime_check.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+ int isprime(int n){
+        if (n<=1)
+        {
+            return 0;
+        }
+        
+        for ( int i = 2; i < n; i++)
+            {
+                if(n%i==0){
+                    return 0;
+                }
+            }
+        return 1;
+        
+    }
+
+
+int main(){
+     int n,r;
+     printf("enter your number: ");
+     scanf("%d",&n);
+     r = isprime(n);
+     if (r==0)
+     {
+        printf("number is not prime");
+     }
+     else {
+        printf("number is prime");
+     }
+     
+return 0;
+
+}
